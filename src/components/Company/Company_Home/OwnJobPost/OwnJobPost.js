f5ca741cfee1dc777bde4babc1b0dd40c27a64ec
Faraz Samin <saminfaraz40@gmail.com>
2021-09-03 00:57:45 +0600

    job post profile completed

--- src/components/Company/Company_Home/OwnJobPost/OwnJobPost.js
+++ src/components/Company/Company_Home/OwnJobPost/OwnJobPost.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { SetToken } from '../../../utilities/setToken';
-import PostDetail from './PostDetail';
+import AppliedUsers from './AppliedUsers';
+
 
 
 
 const OwnJobPost = () => {
     const [myJobPosts, setMyJobPosts] = useState([])
+    const handleDeleteJobPost = () => {
+        SetToken(localStorage.getItem('companyToken'));
+        axios.delete('https://iiuc-campus-recuitement-system.herokuapp.com/')
+            .then(response => {
+                console.log(response.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
     useEffect(() => {
         SetToken(localStorage.getItem('companyToken'));
         axios.get('https://iiuc-campus-recuitement-system.herokuapp.com/job/myJobsPost')
@@ -23,10 +34,31 @@ const OwnJobPost = () => {
             <div className="col-md-9 m-auto">
                 <h1 className="text-center">My Job Posts</h1>
                 {
-                    myJobPosts.map(post => <PostDetail post={post}></PostDetail>)
+                    myJobPosts.map(post => {
+                        const { title, description, _id } = post;
+                        return (
+                            <div className="container">
+                                <div class="card w-75 mt-3">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{title}</h5>
+                                        <p class="card-text">{description}</p>
+                                        <button onClick={handleDeleteJobPost} className="btn btn-danger">Delete</button>
+                                        {/* <a style={{ marginLeft: '3%' }} href="f" class="btn btn-primary">Edit</a> */}
+                                        <div>
+                                            <h2 className="text-center mt-2 mb-2">Applied Users</h2>
+                                            <AppliedUsers _id={_id}></AppliedUsers>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        )
+
+                    })
                 }
             </div>
-        </div>
+        </div >
     );
 };
 
--- src/components/SeeUserProfile/SeeUserProfile.js
+++ src/components/SeeUserProfile/SeeUserProfile.js
@@ -8,12 +8,21 @@ const SeeUserProfile = () => {
     let { _id } = useParams();
     const [userInfo, setUserInfo] = useState([])
     const [experience, setExperience] = useState([])
+    const [profilePic, setProfilePic] = useState([])
+    const [skills, setSkills] = useState([])
+    const [info, setInfo] = useState([])
+    const [socialInfo, setSocialInfo] = useState()
     useEffect(() => {
         SetToken(localStorage.getItem('companyToken'));
         axios.get(`https://iiuc-campus-recuitement-system.herokuapp.com/profile/${_id}`)
             .then(response => {
                 setUserInfo(response.data)
                 setExperience(response.data.experience)
+                setProfilePic(response.data.profilePic.data)
+                setSkills(response.data.skills)
+                setInfo(response.data.user)
+                setSocialInfo(response.data.social)
+                // setEducation(response.data.education)
                 console.log(response)
 
             })
@@ -22,19 +31,30 @@ const SeeUserProfile = () => {
             })
     }, [])
 
+    let img = new Buffer.from(profilePic).toString('base64');
+    img = `data:image/jpg;base64,${img}`;
+
     const { bio, status, codeforceusername, company, githubusername, location, website } = userInfo
+    const { name, email } = info;
+    // const { instagram } = socialInfo
     return (
         <div className="row mt-5">
             <div className="col-md-9 m-auto text-center" style={{ boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px' }}>
+                <img style={{ margin: '1%', height: '20%', width: '20%', boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px' }} className="rounded-circle" src={img} alt="" />
+                <h2 className="mb-3 mt-4">Hi, I am {name}</h2>
+                <h5>{status}</h5>
                 <div style={{ boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px', margin: '2% 5%', padding: '1%' }}>
-                    <div className="row m-3">
+
+                    <div style={{ boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px', margin: '2% 5%', padding: '1%' }} className="row m-3">
                         <div style={{ color: '#050505', fontFamily: 'Lato,Arial,Helvetica,sans-serif', fontSize: '1.17em', fontWeight: 'bold', borderRight: '1px solid lightgray' }} className="col-sm-6">
-                            <p>Status</p>
+
                             <p>Bio </p>
                             <p>Current Company </p>
+                            {/* <p>LinkedIn</p> */}
                             <p>Codeforeces Handle </p>
                             <p>Github User Name</p>
                             <p>Website</p>
+                            <p>Email</p>
                             <p>Location</p>
                             <br />
                         </div>
@@ -42,19 +62,33 @@ const SeeUserProfile = () => {
                             <p>{status}</p>
                             <p>{bio}</p>
                             <p>{company}</p>
-                            <p>{codeforceusername}</p>
-                            <p>{githubusername}</p>
-                            <p>{website}</p>
+                            {/* <p>{instagram}</p> */}
+                            <p><a href={codeforceusername}>{codeforceusername}</a></p>
+                            <p><a href={githubusername}>{githubusername}</a></p>
+                            <p><a href={website}>{website}</a></p>
+                            <p><a href={email}>{email}</a></p>
                             <p>{location}</p>
                             <br />
                         </div>
+                    </div>
+                    <div style={{ boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px', margin: '2% 5%', padding: '1%' }}>
+                        <h3>Skills</h3>
+                        {
+                            skills.map(sk => {
+                                return (
+                                    <div>
+                                        <button className="btn btn-secondary m-2">{sk}</button>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                     <h3 style={{ color: '#050505', fontFamily: 'Lato,Arial,Helvetica,sans-serif', fontSize: '1.57em', fontWeight: 'bold' }} className="text-center mt-5">Experience</h3>
                     {
                         experience.map(ex => {
                             const { company, title, description, from, to, location } = ex
                             return (
-                                <div className="row mt-5">
+                                <div style={{ boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px', margin: '2% 5%', padding: '1%' }} className="row mt-5">
                                     <div style={{ borderRight: '1px solid lightgray' }} className="col-sm-6">
                                         <p>Company</p>
                                         <p>Title</p>
@@ -80,6 +114,9 @@ const SeeUserProfile = () => {
 
 
                 </div>
+
+
+
             </div>
         </div>
     );
