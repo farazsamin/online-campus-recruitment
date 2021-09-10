9176af827595d717e972e02721da8512c767972c
Faraz Samin <saminfaraz40@gmail.com>
2021-09-10 22:00:07 +0600

    cf gh profile added

--- src/components/Student/CV/CV.js
+++ src/components/Student/CV/CV.js
@@ -13,6 +13,9 @@ const ShowPic = () => {
     const [experience, setExperience] = useState([])
     const [codeforces, setCodeforces] = useState([])
     const [github, setGithub] = useState([])
+    const [codeforcesInfo, setCodeforcesInfo] = useState([])
+    const [cf, setCf] = useState(false)
+    const [gh, setGh] = useState(false)
 
     useEffect(() => {
         SetToken(localStorage.getItem('userToken'));
@@ -28,6 +31,22 @@ const ShowPic = () => {
         axios.get('https://iiuc-campus-recuitement-system.herokuapp.com/profile/me/codeforceRatings')
             .then(response => {
                 setCodeforces(response.data.result)
+                console.log(response)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        axios.get('https://iiuc-campus-recuitement-system.herokuapp.com/profile/me/codeforceUserInfo')
+            .then(response => {
+                setCodeforcesInfo(response.data.result[0])
+                // console.log(response.data.result[0])
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        axios.get('https://iiuc-campus-recuitement-system.herokuapp.com/user/codeforceAllProblemSet')
+            .then(response => {
+                // setCodeforces(response.data.result)
                 console.log(response)
             })
             .catch(err => {
@@ -56,6 +75,13 @@ const ShowPic = () => {
             })
     }, [])
 
+    const handlecfclick = () => {
+        setCf(!cf)
+    }
+    const handleghclick = () => {
+        setGh(!gh)
+    }
+
     let img = new Buffer.from(profilePic).toString('base64');
     img = `data:image/jpg;base64,${img}`;
 
@@ -63,6 +89,7 @@ const ShowPic = () => {
     const { facebook, instagram } = socialInfo;
     const { school, college, university, fieldOfStudy } = education;
     const { description, from, to, title } = experience;
+    const { avatar, maxRank, maxRating, handle, rank, rating } = codeforcesInfo;
 
     return (
         <div className="row mt-3 mb-5">
@@ -103,6 +130,29 @@ const ShowPic = () => {
                     <h2>Skills</h2>
                     <p>{skills + ' '}</p>
                     <br />
+                </div>
+
+                <div style={{ boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px', margin: '2% 5%', padding: '1%' }}>
+                    <h2>Codeforces Info</h2>
+                    <div className="row mt-3">
+                        <div className="col-md-6 divider">
+                            <p>Handle</p>
+                            <p>Rank</p>
+                            <p>MaxRank</p>
+                            <p>Rating</p>
+                            <p>MaxRating</p>
+
+                        </div>
+                        <div style={{ fontWeight: 'bolder' }} className="col-md-6">
+                            <p>{handle}</p>
+                            <p>{rank}</p>
+                            <p>{maxRank}</p>
+                            <p>{rating}</p>
+                            <p>{maxRating}</p>
+                        </div>
+                    </div>
+
+
                 </div>
 
 
@@ -150,32 +200,34 @@ const ShowPic = () => {
                 </div>
 
                 <div style={{ boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px', margin: '2% 5%', padding: '1%' }}>
-                    <h2>Codeforces Rating</h2>
-
+                    <button className="btn btn-success" onClick={handlecfclick}>Particpated Codeforces Contests</button>
                     {
-                        codeforces.slice(0).reverse().map(cd => {
-                            return (
-                                <div style={{ boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px', margin: '2% 5%', padding: '1%' }}>
-                                    <p>Current Rating : {cd.newRating}</p>
-                                </div>
-                            )
-                        })
+                        cf ?
+                            codeforces.slice(0).reverse().map(cd => {
+                                return (
+                                    <div style={{ boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px', margin: '2% 5%', padding: '1%' }}>
+                                        <p>Current Rating : {cd.newRating}</p>
+                                    </div>
+                                )
+                            }) : <div></div>
                     }
+
 
                 </div>
                 <div style={{ boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px', margin: '2% 5%', padding: '1%' }}>
-                    <h2>Github Repositories</h2>
+                    <button className="btn btn-success" onClick={handleghclick}>Github Projects</button>
 
                     {
-                        github.map(gh => {
-                            return (
-                                <div style={{ boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px', margin: '2% 5%', padding: '1%' }}>
-                                    <p>Project Name : {gh.name}</p>
-                                    <a href={gh.clone_url} target="_blank">Clone</a>
+                        gh ?
+                            github.map(gh => {
+                                return (
+                                    <div style={{ boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px', margin: '2% 5%', padding: '1%' }}>
+                                        <p>Project Name : {gh.name}</p>
+                                        <a href={gh.clone_url} target="_blank">Clone</a>
 
-                                </div>
-                            )
-                        })
+                                    </div>
+                                )
+                            }) : <div></div>
                     }
 
                 </div>
